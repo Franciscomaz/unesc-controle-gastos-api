b35bd58577e3023d147843cb404ecd629f7d701d
Francisco <chicomazfilho@hotmail.com>
2019-10-16 21:53:29 -0300

    Adiciona entidade para etiquetas do lançamento

---
+++ src/models/lancamento-etiqueta.js
@@ -0,0 +1,21 @@
+const Mongoose = require('mongoose');
+
+const LancamentoSchema = new Mongoose.Schema(
+  {
+    lancamento: {
+      type: Mongoose.Schema.Types.ObjectId,
+      required: [true, 'É necessário informar o lançamento'],
+      index: true,
+      ref: 'lancamentos'
+    },
+    etiqueta: {
+      type: Mongoose.Schema.Types.ObjectId,
+      required: [true, 'É necessário informar a conta'],
+      index: true,
+      ref: 'etiquetas'
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = Mongoose.model('lancamentos_etiquetas', LancamentoSchema);
