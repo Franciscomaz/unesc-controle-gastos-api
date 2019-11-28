1a43420d01e2015afc57ac0d576816733f7d8f56
Francisco <chicomazfilho@hotmail.com>
2019-11-27 21:40:41 -0300

    Atualiza swagger

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
 
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('Servidor iniciado na porta', port));
